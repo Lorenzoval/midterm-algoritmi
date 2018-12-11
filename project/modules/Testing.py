9f08e3b5b951369726d7a8fc1a6b0ac566f9f908
Lorenzo Valeriani <lorenzoval98@gmail.com>
2018-12-11 18:00:07 +0100

    Add functions for testing purpose

---
+++ project/modules/Testing.py
@@ -0,0 +1,28 @@
+import random
+
+
+def generateRandList(steps, maxValue=None):
+    if maxValue is None:
+        maxValue = steps
+    retList = []
+    for _ in range(steps):  # @UnusedVariable
+        retList.append(random.randint(0, maxValue))
+
+    return retList
+
+
+def generateSortedList(stop, start=0, step=1):
+    return list(range(start, stop, step))
+
+
+def generateRevSortedList(start, stop=-1, step=-1):
+    return list(range(start, stop, step))
+
+
+if __name__=="__main__":
+    l = generateRandList(10, 5)
+    print(l)
+    m = generateSortedList(10)
+    print(m)
+    n = generateRevSortedList(10)
+    print(n)
