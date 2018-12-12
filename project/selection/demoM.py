e3d39bd622619a4f1db94a053a913d4209ffc47e
Lorenzo Valeriani <lorenzoval98@gmail.com>
2018-12-12 10:33:59 +0100

    demoM: add avgBestCaseM

--- project/selection/demoM.py
+++ project/selection/demoM.py
@@ -8,15 +8,45 @@ def testSMSForM(l, k, stopM, startM=1):
         print("sampleMedianSelect with m={} takes {} seconds".format(i, selectionTest(l, k, sampleMedianSelect, i)))
 
 
+def minKeyDict(d):
+    val = list(d.values())
+    m = min(val)
+    for k in d:
+        if d[k] == m:
+            return k
+
+
+def avgBestCaseM(l, k, num, stopM, startM=1):
+    """
+    Popola un dizionario myDict con (chiave = m, valore = tempo medio di esecuzione su num tests).
+    Restituisce la chiave con valore minimo.
+    """
+    myDict = {}
+    keys = range(startM, stopM)
+    values = [0 for _ in keys]
+    for k in keys:
+        myDict[k] = values[k - startM]
+    for _ in range(num):
+        for i in range(startM, stopM):
+            myDict[i] += selectionTest(l, k, sampleMedianSelect, i)
+    for j in keys:
+        myDict[j] = myDict[j] / num
+    return minKeyDict(myDict)  # oppure return min(myDict, key=myDict.get)
+
+
 if __name__=="__main__":
-    print("List with 250000 elements in ascending order:")
-    ordered = generateSortedList(250000)
+    print("List with 25000 elements in ascending order:")
+    ordered = generateSortedList(25000)
     testSMSForM(ordered[:], ceil(len(ordered) / 2), 50)
-    print("List with 250000 elements in descending order:")
-    reverse = generateRevSortedList(249999)
+    print("Launching 20 tests, on average, the best case was achieved with m =",
+           avgBestCaseM(ordered[:], ceil(len(ordered) / 2), 20, 50))
+    print("List with 25000 elements in descending order:")
+    reverse = generateRevSortedList(24999)
     testSMSForM(reverse[:], ceil(len(reverse) / 2), 50)
-    print("List with 250000 random elements:")
-    rand = generateRandList(250000)
+    print("Launching 20 tests, on average, the best case was achieved with m =",
+           avgBestCaseM(reverse[:], ceil(len(reverse) / 2), 20, 50))
+    print("List with 25000 random elements:")
+    rand = generateRandList(25000)
     testSMSForM(rand[:], ceil(len(rand) / 2), 50)
-
-
+    print("Launching 20 tests, on average, the best case was achieved with m =",
+           avgBestCaseM(rand[:], ceil(len(rand) / 2), 20, 50))
